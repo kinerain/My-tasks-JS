19ed584663964ed486f5543933aa41490a954aed
ASTAG <defstrouk104@mail.ru>
2024-03-30 17:11:37 +0300

    my comment

---
+++ task.js
@@ -0,0 +1,75 @@
+
+// /* article 6 */
+// alert("Hi, bro...") // уведомление, которое будет выскакивать при открытии страницы
+// let userName = 'Hunk'; // ввод значений для вывода в консоли браузера
+// let userAge = 17;
+// userHeight = 187;
+// console.log(userName + 'is' + userAge + 'years old' + userHeight); // вывод значений в консоли браузера
+// console.log(38*38); // 38*38 = 1444
+// console.log(16*16); // 16*16 = 256
+// console.log(20*20); // 20*20 = 400
+// console.log(`289+34=${289+34}`); // 289+34 = 323
+// // boolean - булевый тип. Используется для значений ложно/истинно
+// let nameFieldChecked = true; // да, поле отмечено
+// let ageFieldChecked = false; // нет, поле не отмечено
+// // помещение переменной в другую
+// let a = 1;
+// let b = a;
+// console.log(a + 1);
+// let counter = 5
+// counter--;
+// console.log(counter); // result = 4
+// console.log( 2**9); // result = 512
+// console.log(2/4);
+// console.log(3+
+// +4); // result = 7
+// console.log(512*712); // result 364544
+// console.log("1" + 325);
+// let x = 2*2+35;
+// console.log(x);
+
+// let z;
+// console.log(z+1);
+
+const temp = -30;
+if (temp<0){
+    console.log("Опасно холодно")
+} if (temp>0 && temp<5){
+    console.log("Холодно")
+} if (temp<0 && temp<-100) {
+    console.log("Очень холодно")
+}
+// const summ = 6000;
+// if (summ >1000 ){
+//    console.log(summ*0.95) 
+// } if (summ >5000 ){
+//     console.log(summ*0.9) 
+//  }
+
+// const age = 17;
+// if (age>18) {
+//     console.log("Взрослый")
+// } if (age<18) {
+//     console.log("Подросток")
+// } if (age>= 65) {
+//     console.log("Пенсионер")
+// }
+
+// const market = 9;
+// if (market>18) {
+//     console.log("Закрыто")
+// } if (market>=9) {
+//     console.log("Открыто")
+// }
+
+// const result = 90;
+// if (result>=90) {
+//     console.log("Отлично")
+// } if (result<90 && result>= 70) {
+//     console.log("Хорошо")
+// } if (result<=69 && result>=50) {
+//     console.log("Удовлетворительно")
+// } if (result<50) {
+//     console.log("Неудачно")
+// }
+
